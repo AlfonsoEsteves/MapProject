c9116914b9d26f45d3ac35eb5721c33ac0227fb4
anunez2 <Alfonso.EstevezNunez@sony.com>
2018-11-09 14:03:10 -0300

    graphics

--- VSProject/Map/Graphics.h
+++ VSProject/Map/Graphics.h
@@ -3,11 +3,11 @@
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 750
 
-#define RESOURCES_PATH string("C:/Users/Usuario/Documents/Projects/MapProject/Resources/")
-//#define RESOURCES_PATH string("C:/Users/alfonso.esteves/Documents/Otros/MapProject/Resources/")
+//#define RESOURCES_PATH string("C:/Users/Usuario/Documents/Projects/MapProject/Resources/")
+#define RESOURCES_PATH string("C:/Users/alfonso.esteves/Documents/Otros/MapProject/Resources/")
 
-#define VIEW_WIDTH 90
-#define VIEW_HEIGHT 90
+#define VIEW_WIDTH 70
+#define VIEW_HEIGHT 70
 
 bool graphics_init();
 
@@ -17,5 +17,8 @@ void graphics_close();
 
 string stepName(int step);
 
+unsigned char safeTilesMap(int x, int y, int z);
+
 extern int screenX[VIEW_WIDTH][VIEW_WIDTH];
 extern int screenY[VIEW_WIDTH][VIEW_WIDTH][VIEW_WIDTH];
+
