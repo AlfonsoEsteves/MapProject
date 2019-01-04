57d0a5b1fd869b74bee88aa440375df2734f4d79
alf <alf@alf.alf>
2019-01-04 00:51:25 -0300

    -

--- VSProject/Map/Parameters.h
+++ VSProject/Map/Parameters.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define SEED 141
+#define SEED 142
 
 #define UNIT_COUNT 100
 
@@ -50,7 +50,7 @@
 #define RESOURCE_CATEGORIES 3
 #define NO_RESOURCE (RESOURCE_TYPES * RESOURCE_CATEGORIES)
 
-#define FAST_FOWARD 225
+#define FAST_FOWARD 1000
 //#define CALCULATE_HASH_AT_TIME 5001
 #define DEBUG true
 #define SAFE true //Unneeded assignment of variables to maintain consistency
@@ -58,7 +58,7 @@
 //#define LOG_PF true
 
 #define DEBUG_TIME 0
-#define DEBUG_OBJECT 85
+#define DEBUG_OBJECT -1
 
 #ifdef DEBUG
 void debug_checkGlobalVariableCorrectness();
